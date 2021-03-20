5a3aa7883b1af07c412173b8ab8f27dd80907c6a
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2021-03-20 22:12:06 +0700

    :lock: Update packages with security vulnerabilities

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,24 @@ const app = require('./app');
 const { connect, connection } = require('./connection');
 
 async function main() {
-    try {
-        await connect()
-    } catch(error) {
-        console.log(error);
-    }
+    // try {
+    //     await connect()
+    //     connection.query(`
+    //         CREATE TABLE IF NOT EXISTS SensorValue (
+    //             id INT NOT NULL AUTO_INCREMENT,
+    //             timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    //             value INT NOT NULL,
+    //             PRIMARY KEY (id)
+    //         );
+    //     `)
+    // } catch (error) {
+    //     console.log(error);
+        // console.log("Retry connecting to database");
+        // setTimeout(() => {
+        //     main();
+        // }, 5000);
+    // }
 
-    connection.query(`
-        CREATE TABLE IF NOT EXISTS SensorValue (
-            id INT NOT NULL AUTO_INCREMENT,
-            timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            value INT NOT NULL,
-            PRIMARY KEY (id)
-        );
-    `)
     app.listen(app.get('port'), () => {
         console.log(`Listening on port ${app.get('port')}`);
     });
